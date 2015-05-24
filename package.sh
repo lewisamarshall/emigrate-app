2fd9ae41325a526753be8c208ea39dbc7f38b0a9
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-05-24 01:09:57 -0700

    Stopped supporting x64 version.

--- package.sh
+++ package.sh
@@ -6,7 +6,6 @@
 # Pack the files into the asar archives.
 asar pack emigrate/ dist/Emigrate.app/Contents/Resources/app.asar
 asar pack emigrate/ dist/Emigrate-win32/resources/app.asar
-asar pack emigrate/ dist/Emigrate-x64/resources/app.asar
 
 # zip -ru dist/Emigrate.app.zip dist/Emigrate.app
 # zip -ru dist/dist/Emigrate-win32.zip dist/Emigrate-win32
